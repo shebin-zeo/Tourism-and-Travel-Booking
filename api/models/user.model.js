b00b76185d7128a3a6d220cc803fbf204d33add6
shebin-zeo <shebin2024@gmail.com>
2025-01-04 15:44:30 +0530

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import { time } from "console";
+import mongoose    from "mongoose";
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true // The unique option is set to true so that the username is unique and cannot be duplicated in the database.
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{timestamps: true}); // The timestamps option is set to true so that the created_at and updated_at fields are automatically added to the document.
+const User = mongoose.model('User', userSchema);
+export default User; // User is the name of the model and userSchema is the schema that defines the model. The model is exported so that it can be used in other parts of the application.
+
+
