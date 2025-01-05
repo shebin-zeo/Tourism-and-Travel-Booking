d6d84da2394641c87b9a21f11e8ca5547b56da12
shebin-zeo <shebin2024@gmail.com>
2025-01-05 18:08:00 +0530

    Add Redux tool kit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from './redux/store.js'  // Import the store from the store.js file.
+import { Provider } from 'react-redux' // Import the Provider from the react-redux library.
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
